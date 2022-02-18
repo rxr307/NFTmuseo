99dfb48a44120e37e003329b593c64e75facf4a4
rodney_russell@hotmail.com <rodney_russell@hotmail.com>
2022-02-18 17:33:55 -0500

    removed djmoney

--- Capstone/gallery_app/models.py
+++ Capstone/gallery_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 from django.db.models.fields import BooleanField, TextField
-from djmoney.models.fields import MoneyField
+
 
 from users_app.models import User
 
